e7a93a806d73962f1a982513b7414d1aeed5b2fd
Maikol Echenique <maikolechenique@gmail.com>
2019-12-14 16:09:33 -0400

    Actualizacion de Funciones

--- application/controllers/api/Admincontrol.php
+++ application/controllers/api/Admincontrol.php
@@ -1,23 +1,98 @@
 <?php
-  
+
 require APPPATH . 'libraries/REST_Controller.php';
-     
+
 class AdminControl extends REST_Controller {
-	
+
 	public $estatus = true;
-	
+
 	public function __construct() {
 		parent::__construct(); //Polimorfismo
-		
-		$this->load->database(); //Establece la conexión con Sysbase		
+
+		$this->load->database(); //Establece la conexión con Sysbase
 	}
 	function listar_get(){
-		
-		$sql = "SELECT pc_iva,ta_dollar,fecha_aplica_dol,mn_petro,fecha_aplica_ptr,mn_euro,fecha_aplica_eur 
+
+		$sql = "SELECT pc_iva,ta_dollar,fecha_aplica_dol,mn_petro,fecha_aplica_ptr,mn_euro,fecha_aplica_eur
 			    FROM view_dashboard";
 		$data = $this->db->query( $sql )->result();
 		$this->response($data, REST_Controller::HTTP_OK);
 	}
-		
+
+  function correlativoseried_get(){
+
+		$sql = "SELECT nu_documento FROM admin_control";
+		$data = $this->db->query( $sql )->result();
+		$this->response($data, REST_Controller::HTTP_OK);
+	}
+
+  function correlativoseriee_get(){
+
+		$sql = "SELECT nu_documento_h FROM admin_control";
+		$data = $this->db->query( $sql )->result();
+		$this->response($data, REST_Controller::HTTP_OK);
+	}
+
+  function correlativoseriec_get(){
+
+    $sql = "SELECT nu_documento_c FROM admin_control";
+    $data = $this->db->query( $sql )->result();
+    $this->response($data, REST_Controller::HTTP_OK);
+  }
+
+  function valorut_get(){
+
+    $sql = "SELECT mn_ut FROM admin_control";
+    $data = $this->db->query( $sql )->result();
+    $this->response($data, REST_Controller::HTTP_OK);
+  }
+
+  function valoriva_get(){
+
+    $sql = "SELECT pc_iva FROM admin_control";
+    $data = $this->db->query( $sql )->result();
+    $this->response($data, REST_Controller::HTTP_OK);
+  }
+
+  function correlativorecibopago_get(){
+
+    $sql = "SELECT nu_pago FROM admin_control";
+    $data = $this->db->query( $sql )->result();
+    $this->response($data, REST_Controller::HTTP_OK);
+  }
+
+  function correlativonotadebito_get(){
+
+    $sql = "SELECT nu_documento_ndb FROM admin_control";
+    $data = $this->db->query( $sql )->result();
+    $this->response($data, REST_Controller::HTTP_OK);
+  }
+  function correlativonotacredito_get(){
+
+    $sql = "SELECT nu_documento_ncr FROM admin_control";
+    $data = $this->db->query( $sql )->result();
+    $this->response($data, REST_Controller::HTTP_OK);
+  }
+
+  function retencioniva_get(){
+
+    $sql = "SELECT pc_retencion FROM admin_control";
+    $data = $this->db->query( $sql )->result();
+    $this->response($data, REST_Controller::HTTP_OK);
+  }
+
+  function tasadolar_get(){
+
+    $sql = "SELECT tasa_de_cambio_dol FROM admin_control";
+    $data = $this->db->query( $sql )->result();
+    $this->response($data, REST_Controller::HTTP_OK);
+  }
+
+  
+
+
+
+
+
 
 }
